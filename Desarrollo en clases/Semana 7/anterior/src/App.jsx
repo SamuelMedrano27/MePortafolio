8bd3859d40c8dad6a6deca76990b8f2650287c55
Samuel Medrano <73202237+SamuelMedrano27@users.noreply.github.com>
2023-05-31 13:03:47 -0500

    semana 7

---
+++ Desarrollo en clases/Semana 7/anterior/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from './componets/Card'
+import Header from './componets/Header'
+
+
+function App() {
+ return(
+
+  <div>
+<h1> Desde componente App</h1>
+<Header nombre="Samuel Medrano" direccion="Av.Jose Galvez 820"/>
+<Card> FIS - UNCP - 2023</Card>
+  </div>
+  )
+}
+
+
+export default App
