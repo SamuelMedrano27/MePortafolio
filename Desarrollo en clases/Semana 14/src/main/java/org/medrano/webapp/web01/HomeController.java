74a20d3fa730f554e44f48e67239c5b4c15a9320
Samuel Medrano <73202237+SamuelMedrano27@users.noreply.github.com>
2023-07-25 00:38:41 -0500

    z5

---
+++ Desarrollo en clases/Semana 14/src/main/java/org/medrano/webapp/web01/HomeController.java
@@ -0,0 +1,13 @@
+package org.medrano.webapp.web01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
---
+++ Desarrollo en clases/Semana 14/src/test/java/org/medrano/webapp/web01/Web01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medrano.webapp.web01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
